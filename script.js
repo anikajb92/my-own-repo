e4c01e8cc443ec49af3e165ada5d5d257ea694bf
Anika Bernstein <anikajb92@gmail.com>
2021-03-11 12:48:51 -0700

    Updated styling and JS functionality on hover

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
 
+const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
+
+portfolioItems.forEach(portfolioItem => {
+    portfolioItem.addEventListener('mouseover', () => {
+        portfolioItem.childNodes[1].classList.add('img-darken');
+        console.log(portfolioItem.childNodes[1].classList);
+    })
+    portfolioItem.addEventListener('mouseout', () => {
+        portfolioItem.childNodes[1].classList.remove('img-darken');
+        console.log(portfolioItem.childNodes[1].classList);
+    })
+})
